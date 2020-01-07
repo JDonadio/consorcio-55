4ca46556c4e0b07f849a25c75bb4e9ec3fd979a2
JDonadio <tanojaja@gmail.com>
2020-01-07 18:54:47 -0300

    add fonts - fix add forms and validations

--- src/app/shared/client-form/client-form.component.ts
+++ src/app/shared/client-form/client-form.component.ts
@@ -46,7 +46,7 @@ export class ClientFormComponent implements OnInit {
       dateContractTo: [''],
       owner: [''],
       consortiums: ['', Validators.required],
-      contractURL: ['', Validators.required],
+      contractURL: [''],
     });
     this.clients = [];
     this.consortiums = [];
@@ -83,23 +83,19 @@ export class ClientFormComponent implements OnInit {
 
     if (isOwner) {
       this.clientForm.get('dateContractFrom').setValidators(null);
-      this.clientForm.get('dateContractFrom').updateValueAndValidity();
       this.clientForm.get('dateContractTo').setValidators(null);
-      this.clientForm.get('dateContractTo').updateValueAndValidity();
       this.clientForm.get('contractURL').setValidators(null);
-      this.clientForm.get('contractURL').updateValueAndValidity();
       this.clientForm.get('owner').setValidators(null);
-      this.clientForm.get('owner').updateValueAndValidity();
     } else {
       this.clientForm.get('dateContractFrom').setValidators([Validators.required]);
-      this.clientForm.get('dateContractFrom').updateValueAndValidity();
       this.clientForm.get('dateContractTo').setValidators([Validators.required]);
-      this.clientForm.get('contractURL').updateValueAndValidity();
       this.clientForm.get('contractURL').setValidators([Validators.required]);
-      this.clientForm.get('dateContractTo').updateValueAndValidity();
       this.clientForm.get('owner').setValidators([Validators.required]);
-      this.clientForm.get('owner').updateValueAndValidity();
     }
+    this.clientForm.get('dateContractFrom').updateValueAndValidity();
+    this.clientForm.get('dateContractTo').updateValueAndValidity();
+    this.clientForm.get('contractURL').updateValueAndValidity();
+    this.clientForm.get('owner').updateValueAndValidity();
   }
 
   public selectedFile(files: FileList) {
@@ -109,14 +105,17 @@ export class ClientFormComponent implements OnInit {
   }
   
   public async addClient() {
+    console.log(this.clientForm);
     this.messagesService.showLoading({ msg: 'Agregando cliente...' });
 
     let name = this.clientForm.get('name').value;
     let lastName = this.clientForm.get('lastName').value;
     let consortium = this.selectedConsortiums.join('-');
     let folderName = `${lastName}-${name}`;
-    let downloadURL = await this.firestoreService.uploadFile(consortium, folderName, this.file);
+    let downloadURL = '';
 
+    if (!this.isOwner) downloadURL = await this.firestoreService.uploadFile(consortium, folderName, this.file);
+    
     let opts = {
       name,
       lastName,
@@ -129,7 +128,7 @@ export class ClientFormComponent implements OnInit {
       cellphone: this.clientForm.get('cellphone').value,
       dateContractFrom: this.clientForm.get('dateContractFrom').value,
       dateContractTo: this.clientForm.get('dateContractTo').value,
-      owner: this.selectedOwner ? this.selectedOwner : null,
+      owner: !this.isOwner && this.selectedOwner ? this.selectedOwner : null,
       consortiums: this.selectedConsortiums[0] ? this.selectedConsortiums : null,
       contractURL: downloadURL,
     }
@@ -148,13 +147,16 @@ export class ClientFormComponent implements OnInit {
     this.clientForm.patchValue({ name: '' });
     this.clientForm.patchValue({ lastName: '' });
     this.clientForm.patchValue({ address: '' });
+    this.clientForm.patchValue({ number: '' });
+    this.clientForm.patchValue({ floor: '' });
+    this.clientForm.patchValue({ apartment: '' });
+    this.clientForm.patchValue({ cellphone: '' });
     this.clientForm.patchValue({ type: 'client' });
     this.clientForm.patchValue({ dateContractFrom: '' });
     this.clientForm.patchValue({ dateContractTo: '' });
-    this.clientForm.patchValue({ owner: '' });
-    this.clientForm.patchValue({ consortiums: '' });
     this.clientForm.patchValue({ contractURL: '' });
-    this.changeOwnership(this.isOwner);
+    this.selectedOwner = null;
+    this.selectedConsortiums = null;
   }
 
   private onSuccess(opts: any) {
