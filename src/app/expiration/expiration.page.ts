11826e682ff4a9dcee03305383a844b587ae1b98
JDonadio <tanojaja@gmail.com>
2020-01-27 15:59:00 -0300

    complete expiration check and styles

--- src/app/expiration/expiration.page.ts
+++ src/app/expiration/expiration.page.ts
@@ -11,6 +11,7 @@ import * as _ from 'lodash';
 })
 export class ExpirationPage implements OnInit {
   public filteredClients: any;
+  public now: any;
   private clients: any;
 
   constructor(
@@ -20,6 +21,7 @@ export class ExpirationPage implements OnInit {
     private zone: NgZone,
   ) {
     this.filteredClients = [];
+    this.now = new Date();
   }
 
   ngOnInit() {
@@ -29,6 +31,7 @@ export class ExpirationPage implements OnInit {
       this.zone.run(() => {
         this.clients = clients;
         console.log('clients:', clients);
+        this.checkExpirationContractDates();
       });
     });
   }
@@ -54,15 +57,48 @@ export class ExpirationPage implements OnInit {
     let res = _.map(expirationDates, d => {
       let diff = Math.ceil((d.date - today) / dayFormater);
 
-      if (diff <= 90) return {
+      let resObj = {
         key: d.key,
-        result: diff
+        diff,
+        class: ''
+      };
+
+      if (diff <= 90) {
+        if (diff <= 0) resObj.class = 'expired';
+        if (diff > 0 && diff <= 30) resObj.class = 'danger';
+        if (diff > 30 && diff <= 60) resObj.class = 'alert';
+        if (diff > 60 && diff <= 90) resObj.class = 'warning';
       }
+
+      return resObj;
     });
 
     setTimeout(() => {
-      this.filteredClients = _.filter(this.clients, c => _.map(res, 'key').includes(c.key));
+      let filteredClients = _.compact(_.map(this.clients, c => {
+        if (_.map(res, 'key').includes(c.key)) return {...c, expiration: _.find(res, r => r.key == c.key)}
+      }));
+
+      console.log('filteredClients:', filteredClients)
+      
+      // Testing purpose
+      
+      // let sim1 = _.cloneDeep(filteredClients[0]);
+      // sim1.expiration.diff = -10;
+      // sim1.expiration.class =  'expired';
+      // let sim2 = _.cloneDeep(filteredClients[0]);
+      // sim2.expiration.diff = 10;
+      // sim2.expiration.class = 'danger';
+      // let sim3 = _.cloneDeep(filteredClients[0]);
+      // sim3.expiration.diff = 40;
+      // sim3.expiration.class = 'alert';
+      // let sim4 = _.cloneDeep(filteredClients[0]);
+      // sim4.expiration.diff = 70;
+      // sim4.expiration.class = 'warning';
+      // filteredClients.concat(sim1, sim2, sim3, sim4);
+
+      this.filteredClients = _.compact(filteredClients);
       console.log('this.filteredClients', this.filteredClients)
+      this.now = new Date();
       this.messagesService.dismissLoading();
     }, 1000);
   }
